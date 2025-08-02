76049986a94a19930a93cd5620003154e8cb8e13
Pomtua <boombeem2121@gmail.con>
2025-08-02 18:46:20 +0700

    fix(models): change primary key and remove gorm.Model

--- models/enrollment.go
+++ models/enrollment.go
@@ -1,11 +1,19 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Enrollment struct {
-	gorm.Model
 	LearnerID        uint   `gorm:"primaryKey"`
+	ClassID          uint   `gorm:"primaryKey"`
 	EnrollmentStatus string `gorm:"size:20"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	Learner Learner `gorm:"foreignKey:LearnerID;references:ID;constraint:OnDelete:CASCADE"`
 	Class   Class   `gorm:"foreignKey:ClassID;references:ID;constraint:OnDelete:CASCADE"`
--- models/review.go
+++ models/review.go
@@ -1,13 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Review struct {
-	gorm.Model
 	LearnerID uint   `gorm:"primaryKey"`
 	ClassID   uint   `gorm:"primaryKey"`
 	Rating    int    `gorm:"not null;check:rating >= 1 AND rating <= 5"`
 	Comment   string `gorm:"size:255"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	Learner Learner `gorm:"foreignKey:LearnerID;references:ID;constraint:OnDelete:CASCADE"`
 	Class   Class   `gorm:"foreignKey:ClassID;references:ID;constraint:OnDelete:CASCADE"`
