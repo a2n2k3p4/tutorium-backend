259c3287358f4d0798ffa13c84fb8bd8369e02e3
pomtua <boombeem2121@gmail.com>
2025-08-22 16:18:40 +0700

    feat(user): add UserID field to Admin, Learner, and Teacher models

--- models/admin.go
+++ models/admin.go
@@ -4,10 +4,12 @@ import "gorm.io/gorm"
 
 type Admin struct {
 	gorm.Model
+	UserID uint `gorm:"unique;not null"`
 }
 
 // ---- DOC-ONLY STRUCT FOR SWAGGER BELOW ----
 
 type AdminDoc struct {
-	ID uint `json:"id" example:"43"`
+	ID     uint `json:"id" example:"43"`
+	UserID uint `json:"user_id" example:"5"`
 }
--- models/learner.go
+++ models/learner.go
@@ -4,10 +4,12 @@ import "gorm.io/gorm"
 
 type Learner struct {
 	gorm.Model
+	UserID uint `gorm:"unique;not null"`
 }
 
 // ---- DOC-ONLY STRUCT FOR SWAGGER BELOW ----
 
 type LearnerDoc struct {
-	ID uint `json:"id" example:"42"`
+	ID     uint `json:"id" example:"42"`
+	UserID uint `json:"user_id" example:"5"`
 }
