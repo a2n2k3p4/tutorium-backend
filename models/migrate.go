3519647783f8d67183b975e6ecc928ff9dc8997d
pomtua <boombeem2121@gmail.com>
2025-08-22 16:19:22 +0700

    fix(migrations): reorder migrations to respect dependencies

--- models/migrate.go
+++ models/migrate.go
@@ -8,19 +8,19 @@ import (
 
 func Migrate(db *gorm.DB) {
 	err := db.AutoMigrate(
+		&User{},
 		&Admin{},
+		&Learner{},
+		&Teacher{},
 		&BanDetailsLearner{},
 		&BanDetailsTeacher{},
 		&Class{},
 		&ClassCategory{},
 		&ClassSession{},
 		&Enrollment{},
-		&Learner{},
 		&Notification{},
 		&Report{},
 		&Review{},
-		&Teacher{},
-		&User{},
 	)
 	if err != nil {
 		log.Fatalf("migration failed: %v", err)
