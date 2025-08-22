f0a6fd4baa4d5359c2448b791dce187e26f53875
pomtua <boombeem2121@gmail.com>
2025-08-22 16:20:37 +0700

    fix(report): set ReportUserID to NULL when reporter is deleted

--- models/report.go
+++ models/report.go
@@ -8,14 +8,14 @@ import (
 
 type Report struct {
 	gorm.Model
-	ReportUserID      uint   `gorm:"not null"`
+	ReportUserID      uint
 	ReportedUserID    uint   `gorm:"not null"`
 	ReportType        string `gorm:"size:20;not null"`
 	ReportDescription string `gorm:"size:255"`
 	ReportPicture     []byte
 	ReportDate        time.Time `gorm:"default:CURRENT_TIMESTAMP"`
 
-	Reporter User `gorm:"foreignKey:ReportUserID;constraint:OnDelete:CASCADE"`
+	Reporter User `gorm:"foreignKey:ReportUserID;constraint:OnDelete:SET NULL"`
 	Reported User `gorm:"foreignKey:ReportedUserID;constraint:OnDelete:CASCADE"`
 }
 
