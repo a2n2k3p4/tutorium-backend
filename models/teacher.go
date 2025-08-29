eceefc16f692669fba28b6cf431d06d573e65fa9
pomtua <boombeem2121@gmail.com>
2025-08-29 16:37:45 +0700

    feat(teacher): add flag_count, ban_count, and email fields

--- models/teacher.go
+++ models/teacher.go
@@ -4,14 +4,20 @@ import "gorm.io/gorm"
 
 type Teacher struct {
 	gorm.Model
-	UserID      uint   `json:"user_id" gorm:"unique;not null"`
-	Description string `json:"description" gorm:"size:255"`
+	UserID      uint    `json:"user_id" gorm:"unique;not null"`
+	Description string  `json:"description" gorm:"size:255"`
+	FlagCount 	int  	`json:"flag_count" gorm:"default:0;not null"`
+	BanCount  	int  	`json:"ban_count" gorm:"default:0;not null"`
+	Email     	string  `json:"email" gorm:"size:100;unique;not null"`
 }
 
 // ---- DOC-ONLY STRUCT FOR SWAGGER BELOW ----
 
 type TeacherDoc struct {
-	ID          uint   `json:"id" example:"12"`
-	UserID      uint   `json:"user_id" example:"5"`
-	Description string `json:"description" example:"Experienced Mathematics teacher specializing in calculus and linear algebra."`
+	ID          uint   	`json:"id" example:"12"`
+	UserID      uint   	`json:"user_id" example:"5"`
+	Description string 	`json:"description" example:"Experienced Mathematics teacher specializing in calculus and linear algebra."`
+	FlagCount 	int  	`json:"flag_count" example:"3"`
+	BanCount  	int  	`json:"ban_count" example:"1"`
+	Email     	string 	`json:"email" example:"teacher@example.com"`
 }
