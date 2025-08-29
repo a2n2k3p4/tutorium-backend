899fbc884b52e8a8d1fe7a90a48cb3853f5f44b1
pomtua <boombeem2121@gmail.com>
2025-08-29 16:38:25 +0700

    fix(user): replace ProfilePicture with ProfilePictureURL

--- models/user.go
+++ models/user.go
@@ -7,13 +7,13 @@ import (
 // Define a struct matching the columns (use pointers for nullable FKs)
 type User struct {
 	gorm.Model
-	StudentID      string  `json:"student_id" gorm:"size:10;uniqueIndex;not null"`
-	ProfilePicture []byte  `json:"profile_picture,omitempty"`
-	FirstName      string  `json:"first_name" gorm:"size:30;not null"`
-	LastName       string  `json:"last_name" gorm:"size:30;not null"`
-	Gender         string  `json:"gender" gorm:"size:6"`
-	PhoneNumber    string  `json:"phone_number" gorm:"size:20"`
-	Balance        float64 `json:"balance" gorm:"type:numeric(12,2);default:0;check:balance >= 0"`
+	StudentID      		string  `json:"student_id" gorm:"size:10;uniqueIndex;not null"`
+	ProfilePictureURL 	string  `json:"profile_picture_url,omitempty"`
+	FirstName      		string  `json:"first_name" gorm:"size:30;not null"`
+	LastName       		string  `json:"last_name" gorm:"size:30;not null"`
+	Gender         		string  `json:"gender" gorm:"size:6"`
+	PhoneNumber    		string  `json:"phone_number" gorm:"size:20"`
+	Balance        		float64 `json:"balance" gorm:"type:numeric(12,2);default:0;check:balance >= 0"`
 
 	Learner *Learner
 	Teacher *Teacher
@@ -23,12 +23,12 @@ type User struct {
 // ---- DOC-ONLY STRUCT FOR SWAGGER BELOW ----
 
 type UserDoc struct {
-	ID             uint    `json:"id" example:"101"`
-	StudentID      string  `json:"student_id" example:"6610505511"`
-	ProfilePicture string  `json:"profile_picture,omitempty" example:"<base64-encoded-image>"`
-	FirstName      string  `json:"first_name" example:"Alice"`
-	LastName       string  `json:"last_name" example:"Smith"`
-	Gender         string  `json:"gender" example:"Female"`
-	PhoneNumber    string  `json:"phone_number" example:"+66912345678"`
-	Balance        float64 `json:"balance" example:"250.75"`
+	ID             		uint    `json:"id" example:"101"`
+	StudentID      		string  `json:"student_id" example:"6610505511"`
+	ProfilePictureURL 	string  `json:"profile_picture_url,omitempty" example:"https://minio.example.com/tutorium/users/101/profile.png"`
+	FirstName      		string  `json:"first_name" example:"Alice"`
+	LastName       		string  `json:"last_name" example:"Smith"`
+	Gender         		string  `json:"gender" example:"Female"`
+	PhoneNumber    		string  `json:"phone_number" example:"+66912345678"`
+	Balance        		float64 `json:"balance" example:"250.75"`
 }
