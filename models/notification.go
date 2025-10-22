57bdb4262295e1715a114ef7a1f1d5c0bda07c69
pomtua <boombeem2121@gmail.com>
2025-10-22 23:51:42 +0700

    feat(notification): add nofitication title field

--- models/notification.go
+++ models/notification.go
@@ -9,6 +9,7 @@ import (
 type Notification struct {
 	gorm.Model
 	UserID                  uint      `json:"user_id" gorm:"not null"`
+	NotificationTitle       string    `json:"notification_title" gorm:"size:100;not null"`
 	NotificationType        string    `json:"notification_type" gorm:"size:30;not null"`
 	NotificationDescription string    `json:"notification_description" gorm:"size:255"`
 	NotificationDate        time.Time `json:"notification_date" gorm:"default:CURRENT_TIMESTAMP"`
@@ -21,6 +22,7 @@ type Notification struct {
 
 type NotificationDoc struct {
 	UserID                  uint      `json:"user_id" example:"42"`
+	NotificationTitle       string    `json:"notification_title" example:"Review received"`
 	NotificationType        string    `json:"notification_type" example:"System Alert"`
 	NotificationDescription string    `json:"notification_description" example:"Your class has been rescheduled"`
 	NotificationDate        time.Time `json:"notification_date" example:"2025-08-20T15:04:05Z"`
